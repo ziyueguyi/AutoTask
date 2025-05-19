b30457f68682ab6a9a7fda8acd266ae1cca1af03
sxzhang1 <sxzhang1@leqee.com>
2025-05-19 17:07:09 +0800

    【功能】新增贴吧签到

--- web/0004——百度贴吧/贴吧.py
+++ web/0004——百度贴吧/贴吧.py
@@ -46,19 +46,27 @@ class PostBar:
             self.config_option.write_config("账户1", "switch", "0")
             self.config_option.write_config("账户1", "cookies", "")
 
-    def run(self):
-        self.initialize.info_message("贴吧签到开始")
-        account_list = self.config_option.read_config_key()
-        for ind, sec in enumerate(account_list):
-            self.initialize.info_message(f"共{len(account_list)}个账户，第{ind + 1}个账户：{sec},")
-            self.session.cookies.update(json.loads(self.config_option.read_config_key(section=sec, key="cookies")))
-            tbs = self.get_tbs()
-            if tbs:
-                time.sleep(random.randint(1, 2))
-                bar_list = self.get_follow_bar()
-                self.sign_bar(bar_list, tbs)
-        self.initialize.info_message("贴吧签到结束")
-        self.initialize.send_notify("贴吧")
+    def get_tbs(self):
+        """
+        验证账号有效性
+        """
+        try:
+            response = self.session.get('http://tieba.baidu.com/dc/common/tbs')
+            return response.json()['tbs']
+        except BaseException as e:
+            self.initialize.error_message(e.__str__())
+            return None
+
+    def get_valid_bar(self, bar_name):
+        """
+        验证贴吧是否有效
+        :param bar_name: 贴吧名称
+        :return:
+        """
+        time.sleep(random.randint(1, 2))
+        params = {'ie': 'utf-8', 'kw': bar_name, 'fr': 'search'}
+        response = self.session.get(f'https://tieba.baidu.com/f', params=params)
+        return response.status_code == 200 and '很抱歉，没有找到相关内容' not in response.text
 
     def get_follow_bar(self):
         """
@@ -80,28 +88,6 @@ class PostBar:
             self.initialize.error_message(e.__str__())
         finally:
             return bar_list
-
-    def get_valid_bar(self, bar_name):
-        """
-        验证贴吧是否有效
-        :param bar_name: 贴吧名称
-        :return:
-        """
-        time.sleep(random.randint(1, 2))
-        params = {'ie': 'utf-8', 'kw': bar_name, 'fr': 'search'}
-        response = self.session.get(f'https://tieba.baidu.com/f', params=params)
-        return response.status_code == 200 and '很抱歉，没有找到相关内容' not in response.text
-
-    def get_tbs(self):
-        """
-        验证账号有效性
-        """
-        try:
-            response = self.session.get('http://tieba.baidu.com/dc/common/tbs')
-            return response.json()['tbs']
-        except BaseException as e:
-            self.initialize.error_message(e.__str__())
-            return None
 
     def sign_bar(self, bar_list, tbs):
         """
@@ -126,6 +112,20 @@ class PostBar:
                     self.initialize.info_message("未知错误", is_flag=True)
             else:
                 self.initialize.error_message(f"贴吧{bl}签到失败", is_flag=True)
+
+    def run(self):
+        self.initialize.info_message("贴吧签到开始")
+        account_list = self.config_option.read_config_key()
+        for ind, sec in enumerate(account_list):
+            self.initialize.info_message(f"共{len(account_list)}个账户，第{ind + 1}个账户：{sec},")
+            self.session.cookies.update(json.loads(self.config_option.read_config_key(section=sec, key="cookies")))
+            tbs = self.get_tbs()
+            if tbs:
+                time.sleep(random.randint(1, 2))
+                bar_list = self.get_follow_bar()
+                self.sign_bar(bar_list, tbs)
+        self.initialize.info_message("贴吧签到结束")
+        self.initialize.send_notify("「贴吧」")
 
 
 if __name__ == '__main__':
